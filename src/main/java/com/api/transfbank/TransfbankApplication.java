2e47e64b1f567ab1e516f8bddad5bd7539291099
Alvaro Campelo <31720844+Alcasi@users.noreply.github.com>
2021-06-07 16:51:32 -0300

    Início do projeto

---
+++ src/main/java/com/api/transfbank/TransfbankApplication.java
@@ -0,0 +1,13 @@
+package com.api.transfbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransfbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransfbankApplication.class, args);
+	}
+
+}
